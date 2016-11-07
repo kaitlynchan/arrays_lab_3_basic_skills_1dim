8e127f3624025483c368e8ec786a716fd521f8a0
Kaitlyn Chan <ununoctium829@gmail.com>
2016-11-06 19:50:00 -0800

    all done :D

--- src/arrays_materials/ArraysLab3.java
+++ src/arrays_materials/ArraysLab3.java
@@ -1,5 +1,4 @@
 package arrays_materials;
-
 import java.util.Arrays;
 
 public class ArraysLab3 
@@ -18,12 +17,10 @@ public class ArraysLab3
 		
 		int [] arrSum = arr1;
 		
-		for (int i = 0; i < arr1.length; i++){
+		for (int i = 0; i < arrSum.length; i++){
 			arrSum[i] += arr2[i];
 		}
 		return arrSum;
-
-		
 	}
 	
 	/*
@@ -37,24 +34,32 @@ public class ArraysLab3
 		assert (arr.length > 0);
 		
 		int [] appendArr = new int [arr.length+1];
-		for (int i = 0; i < appendArr.length; i++){
+		for (int i = 0; i < appendArr.length-1; i++){
 			appendArr[i] = arr[i];
 		}
-		appendArr[appendArr.length] = num;
+		appendArr[appendArr.length-1] = num;
 		return appendArr;
-
-		
 	}
 	
 	/*
 	 * Write a method named remove that accepts an array of integers arr and an integer idx 
 	 * and returns an array of integers consisting of all of the elements of arr 
-	 * except for the element at index idx (thus, the returned array has a length of arr.length – 1).  
+	 * except for the element at index idx (thus, the returned array has a length of arr.length ï¿½ 1).  
 	 * You can assume arr has at least two elements.
 	 */
 	public static int[] remove(int[] arr, int idx)
 	{
 		assert (arr.length >= 2);
+		int [] removeArray = new int [arr.length-1];
+		for (int i = 0; i <removeArray.length;i++){
+			if (i < idx){
+				removeArray[i] = arr[i];
+			}
+			else {
+				removeArray[i] = arr [i+1];
+			}
+		}
+		return removeArray;
 		
 	}
 	
@@ -66,44 +71,32 @@ public class ArraysLab3
 	public static int sumEven(int[] arr)
 	{
 		assert (arr.length > 0);
-		
+		int evenSum = 0;
+		for (int i = 0; i < arr.length; i += 2){
+			evenSum += arr[i];
+		}
+		return evenSum;
 	}
 	
 	/*
 	 * Write a method rotateRight that accepts an array of integers arr 
 	 * and does not return a value.  
 	 * The rotateRight method moves each element of arr one index to the right 
-	 * (element 0 goes to element 1, element 1 goes to element 2, …, element n-1 goes to element 0).  
+	 * (element 0 goes to element 1, element 1 goes to element 2, ï¿½, element n-1 goes to element 0).  
 	 * You can assume arr has at least one element.
 	 */
 	public static void rotateRight(int[] arr)
 	{
 		assert (arr.length > 0);
+		int newFirst = arr[arr.length-1];
+		for (int i = arr.length-1; i > 0; i--){
+			arr[i] = arr[i-1];
+		}
+		arr[0] = newFirst;
 		
 	}
 
-	/*
-	 * Write a main method that will 
-		Contain
-			integer arrays a1, a2, sumArr, appendArr, removeArr
-			int variables appendNum, removeIdx, sumOfEvens
-		Carry out the following actions
-			Declare array a1 containing the values (5, 10, 15, 20, 25, 30, 35, 40) 
-			and array a2 containing the values (7, 14, 21, 28, 35, 42, 49, 56)
-			Form sumArr by calling the sum method with a1 and a2 as inputs
-			Declare int appendNum and set it to 200.
-			Form appendArr by calling the append method with a1 and appendNum as inputs
-			Declare int removeIdx and set it to 5.
-			Form removeArr by calling remove with a2 and removeIdx
-			Form sumOfEvens by calling sumEven with appendArr as the input
-			Call rotateRight with a1 as the input
-			On their own lines print out (use Arrays.toString to transform arrays into a printable strings) :
-				sumArr
-				appendArr
-				removeArr
-				sumOfEvens (an int you don’t need Arrays.toString)
-				a1
-	 */
+	
 	
 	public static void main(String[] args) 
 	{
@@ -129,3 +122,5 @@ public class ArraysLab3
 		
 	}
 }
+
+
